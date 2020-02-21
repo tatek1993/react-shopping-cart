76e971f313108f787552cce7f08bc9d54ab7fd63
Tate Keller <tatek1993@gmail.com>
2020-02-20 22:43:05 -0600

    used CartContext to provide data to ShoppingCart and Navigation, debugging render errors

--- src/App.js
+++ src/App.js
@@ -8,32 +8,35 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState(products[0]);
 
-	const addItem = item => dispatch => {
+	const addItem = item => {
 		// add the given item to the cart
-		dispatch ({type: ADD_ITEM, payload: item.id});
+	
 	};
 
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
+				<CartContext value={cart}>
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route
-					exact
-					path="/"
-					component={Products}
-				/>
+					{/* Routes */}
+					<Route
+						exact
+						path="/"
+						component={Products}
+					/>
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
+					<Route
+						path="/cart"
+						component={ShoppingCart}
+					/>
+				</CartContext>	
 			</ProductContext.Provider>
 		</div>
 	);
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,3 +1,5 @@
 import React, {createContext} from 'react';
 
-export const ProductContext = createContext();
+const ProductContext = createContext();
+
+export default ProductContext;
